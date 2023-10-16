1bd05a1aa170d44d7e7e68de747b85c21419edb7
David <beltrangonzalezdavid@gmail.com>
2023-10-15 23:22:03 -0500

    get events fix

--- controllers/calendar_events.js
+++ controllers/calendar_events.js
@@ -48,7 +48,7 @@ export const createCalendarEvent = async (req, res) => {
 
 export const getCalendarEvents = async (req, res) => {
     try {
-        const { patientId } = req.query;
+        const { doctorId, patientId } = req.query;
 
         // Get the current date to determine the start and end of the month
         const today = new Date();
@@ -67,6 +67,10 @@ export const getCalendarEvents = async (req, res) => {
             },
         };
 
+        if (doctorId) {
+            filters._id_doctor = doctorId;
+        }
+
         if (patientId) {
             filters._id_patient = patientId;
         }
@@ -79,124 +83,6 @@ export const getCalendarEvents = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "Failed to retrieve calendar events",
-            error: error.message,
-        });
-    }
-};
-
-export const updateCalendarEvent = async (req, res) => {
-    try {
-        const { _id_calendar_event } = req.query;
-        const {
-            title,
-            start_date,
-            end_date,
-            description,
-            _id_doctor,
-            _id_patient,
-        } = req.body;
-
-        // Validate request body
-        if (!title || !start_date || !end_date) {
-            return res.status(400).json({
-                success: false,
-                message: "Title, start date, and end date are required",
-            });
-        }
-
-        // Perform additional validations if needed
-
-        const updatedCalendarEvent = await CalendarEvent.update(
-            {
-                title,
-                start_date,
-                end_date,
-                description,
-                _id_doctor,
-                _id_patient,
-            },
-            { where: { _id_calendar_event } }
-        );
-
-        return res.status(200).json({
-            success: true,
-            message: "Calendar event updated successfully",
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to update calendar event",
-            error: error.message,
-        });
-    }
-};
-
-export const deleteCalendarEvent = async (req, res) => {
-    try {
-        const { _id_calendar_event } = req.query;
-
-        const deletedCalendarEvent = await CalendarEvent.destroy({
-            where: { _id_calendar_event },
-        });
-
-        if (!deletedCalendarEvent) {
-            return res
-                .status(404)
-                .json({ success: false, message: "Calendar event not found" });
-        }
-
-        return res.status(200).json({
-            success: true,
-            message: "Calendar event deleted successfully",
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to delete calendar event",
-            error: error.message,
-        });
-    }
-};
-
-export const getClosestEventByDate = async (req, res) => {
-    try {
-        const { _id_doctor } = req.query;
-
-        // Validate if _id_doctor is provided
-        if (!_id_doctor) {
-            return res.status(400).json({
-                success: false,
-                message: "_id_doctor parameter is required",
-            });
-        }
-
-        const closestEvent = await CalendarEvent.findOne({
-            where: {
-                _id_doctor,
-                start_date: {
-                    [Op.gte]: new Date(),
-                },
-            },
-            order: [
-                ["start_date", "ASC"], // Sort by start date in ascending order
-            ],
-        });
-
-        if (!closestEvent) {
-            return res.status(404).json({
-                success: false,
-                message: "No events found for the specified doctor",
-            });
-        }
-
-        return res.status(200).json({ success: true, closestEvent });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to get closest event",
             error: error.message,
         });
     }
