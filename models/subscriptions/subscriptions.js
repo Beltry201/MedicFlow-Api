233c319646902c406910ad36edd12e7f689125a6
David <beltrangonzalezdavid@gmail.com>
2023-11-10 13:46:45 -0500

    Tables preparation

--- models/subscriptions/subscriptions.js
+++ models/subscriptions/subscriptions.js
@@ -1,47 +1,47 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../../config/db.js";
-import { User } from "../users/users.js";
+// import { DataTypes } from "sequelize";
+// import { sequelize } from "../../config/db.js";
+// import { User } from "../users/users.js";
 
-export const Subscription = sequelize.define(
-    "Subscriptions",
-    {
-        _id_subscription_record: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        subscription_start_date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        subscription_end_date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        state: {
-            type: DataTypes.ENUM(
-                "Active",
-                "Pending",
-                "Trial",
-                "Cancelled",
-                "Suspended",
-                "Expired",
-                "Grace Period",
-                "Pending Cancellation",
-                "Free Tier"
-            ),
-            allowNull: false,
-        },
-        is_active: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-    },
-    {
-        tableName: "subscriptions",
-        timestamps: true,
-    }
-);
+// export const Subscription = sequelize.define(
+//     "Subscriptions",
+//     {
+//         _id_subscription_record: {
+//             type: DataTypes.UUID,
+//             defaultValue: DataTypes.UUIDV4,
+//             primaryKey: true,
+//         },
+//         subscription_start_date: {
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//         },
+//         subscription_end_date: {
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//         },
+//         state: {
+//             type: DataTypes.ENUM(
+//                 "Active",
+//                 "Pending",
+//                 "Trial",
+//                 "Cancelled",
+//                 "Suspended",
+//                 "Expired",
+//                 "Grace Period",
+//                 "Pending Cancellation",
+//                 "Free Tier"
+//             ),
+//             allowNull: false,
+//         },
+//         is_active: {
+//             type: DataTypes.BOOLEAN,
+//             allowNull: false,
+//         },
+//     },
+//     {
+//         tableName: "subscriptions",
+//         timestamps: true,
+//     }
+// );
 
-// Define associations
-Subscription.belongsTo(User, { foreignKey: "_id_user" });
+// // Define associations
+// Subscription.belongsTo(User, { foreignKey: "_id_user" });
